0a57830043d244e54115c8411f5208a53f06c065
Victor Johnson <89293077+Victor-Johnson@users.noreply.github.com>
2023-07-12 06:52:39 +0100

    Update create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-
-# Collect all the .c files in the current directory
-c_files=$(find . -maxdepth 1 -type f -name "*.c")
-
-# Compile each .c file and generate corresponding object files
-for file in $c_files; do
-    gcc -c "$file" -o "${file%.c}.o"
-done
-
-# Create the static library using ar
-ar rcs liball.a *.o
-
-# Clean up the generated object files
-rm -f *.o
+gcc -Wall -pedantic -Werror -Wextra -c *.c
+ar -rc liball.a *.o
+ranlib liball.a
